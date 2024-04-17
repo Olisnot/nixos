339893ac429eadb22fef9347bb364def6c02796f
Olisnot <oliverpm2007@gmail.com>
2024-04-17 21:31:24 +0100

    rebuild

--- configuration.nix
+++ configuration.nix
@@ -27,7 +27,7 @@ nix.settings.experimental-features = ["nix-command" "flakes"];
 # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
 # Enable networking
-		networking.networkmanager.enable = true;
+		#networking.networkmanager.enable = true;
 # Enable IWD
     networking.wireless.iwd.enable = true;
     networking.wireless.iwd.settings = {
