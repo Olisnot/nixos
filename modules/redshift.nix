22e742170721d045a8213bd7328af0094d2e3969
Olisnot <oliverpm2007@gmail.com>
2024-04-24 18:49:34 +0100

    rebuild

--- modules/redshift.nix
+++ modules/redshift.nix
@@ -1,15 +1,20 @@
 { pkgs, config, libs, ... }:
 
 {
-    services.redshift = {
-        enable = true;
-        brightness = {
-            day = "1";
-            night = "1";
-        };
-        temperature = {
-            day = 5500;
-            night = 3700;
+    {
+# location.provider = "geoclue2"
+# All values except 'enable' are optional.
+        services.redshift = {
+            enable = true;
+            brightness = {
+# Note the string values below.
+                day = "1";
+                night = "1";
+            };
+            temperature = {
+                day = 5500;
+                night = 3700;
+            };
         };
     };
-};
+}
