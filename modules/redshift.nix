4929a12ccf6a2d07c97a66ecbdec6d5a150366c4
Olisnot <oliverpm2007@gmail.com>
2024-04-24 18:36:41 +0100

    rebuild

--- modules/redshift.nix
+++ modules/redshift.nix
@@ -1,16 +1,15 @@
-{ config, pkgs, callPackage, ... }:
+{ pkgs, config, libs, ... }:
 
 {
-services.redshift = {
-    enable = true;
-    brightness = {
-      # Note the string values below.
-      day = "1";
-      night = "1";
+    services.redshift = {
+        enable = true;
+        brightness = {
+            day = "1";
+            night = "1";
+        };
+        temperature = {
+            day = 5500;
+            night = 3700;
+        };
     };
-    temperature = {
-      day = 5500;
-      night = 3700;
-    };
-  };
 }
