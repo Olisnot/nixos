e58df38f1bd41304b2a7f0fd6b507bc5c636e806
Olisnot <oliverpm2007@gmail.com>
2024-05-05 15:57:36 +0100

    rebuild

--- configuration.nix
+++ configuration.nix
@@ -96,17 +96,6 @@ nix.settings.experimental-features = ["nix-command" "flakes"];
 		xkbVariant = "";
 	};
 
-    services.xserver.displayManager = {
-        defaultSession = "plasma5+i3";
-        session = [
-        {
-            manage = "desktop";
-            name = "plasma5+i3+whatever";
-            start = ''exec env KDEWM=${pkgs.i3}/bin/i3 ${pkgs.plasma-workspace}/bin/startplasma-x11'';
-        }
-        ];
-    };
-
 # Enable CUPS to print documents.
 	services.printing.enable = true;
 
