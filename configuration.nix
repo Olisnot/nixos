25e44785fe9a5bae9958b71ec886376bd4c51cd8
Olisnot <oliverpm2007@gmail.com>
2024-05-13 22:01:01 +0100

    rebuild

--- configuration.nix
+++ configuration.nix
@@ -129,6 +129,9 @@ nix.settings.experimental-features = ["nix-command" "flakes"];
 # Enable flatpak
 services.flatpak.enable = true;
 
+environment.sessionVariables = rec {
+    DOTNET_CLI_TELEMETRY_OPTOUT = 1;
+  };
 
 # Some programs need SUID wrappers, can be configured further or are
 # started in user sessions.
