711ed3e3bf0214f2ec17c49606d2b40c2579d589
Olisnot <oliverpm2007@gmail.com>
2024-05-18 21:51:26 +0100

    rebuild

--- modules/virt-manager.nix
+++ modules/virt-manager.nix
@@ -4,5 +4,14 @@
     virtualisation.libvirtd.enable = true;
     programs.virt-manager.enable = true;
 
+    dconf.settings = {
+        "org/virt-manager/virt-manager/connections" = {
+            autoconnect = ["qemu:///system"];
+            uris = ["qemu:///system"];
+        };
+    };
 
+    {
+       users.users.<oliver>.extraGroups = [ "libvirtd" ];
+    }
 }
