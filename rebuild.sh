e43fbdde095f98d301aba2c9ad01760a35756a5c
Olisnot <oliverpm2007@gmail.com>
2024-06-07 20:11:35 +0100

    rebuild-07-06-2024 20:11:35

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@
 cd /nixos
 sudo git add .
 sudo git commit -m "rebuild-$(date +"%d-%m-%Y %H:%M:%S")"
-sudo nixos-rebuild switch --flake /nixos#default
+sudo nixos-rebuild switch --impure --flake /nixos#default
