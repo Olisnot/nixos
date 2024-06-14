4576c9686c12a153a469a6a78b4c18aa49165911
Olisnot <oliverpm2007@gmail.com>
2024-06-14 20:57:33 +0100

    rebuild-14-06-2024 20:57:33

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,7 @@ nix.settings.experimental-features = ["nix-command" "flakes"];
 # Enable sound with pipewire.
 	sound.enable = true;
 	security.rtkit.enable = true;
+    hardware.pulseaudio.enable = false;
 	services.pipewire = {
 		enable = true;
 		alsa.enable = true;
