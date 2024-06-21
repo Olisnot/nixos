42fcb656b95ab4818ac099a9c62cfab111138a6c
Olisnot <oliverpm2007@gmail.com>
2024-06-22 00:37:41 +0100

    rebuild-22-06-2024 00:37:41

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,8 @@ nix.settings.experimental-features = ["nix-command" "flakes"];
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
 
+    boot.loader.systemd-boot.configurationLimit = 5;
+
 	networking.hostName = "nixos"; # Define your hostname.
 # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     
