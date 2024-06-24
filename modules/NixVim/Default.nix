ad7e142b010d6a8fedb2ee5eda853c85c0203f25
Olisnot <oliverpm2007@gmail.com>
2024-06-24 22:17:20 +0100

    rebuild-24-06-2024 22:17:20

--- modules/NixVim/Default.nix
+++ modules/NixVim/Default.nix
@@ -1,23 +1,24 @@
 { pkgs, inputs, ... }: 
 
 {
-    programs.nixvim = {
-        enable = true;
-        enableMan = true;
-        colorschemes.kanagawa.enable = true;
+	programs.nixvim = {
+		enable = true;
+		enableMan = true;
+		colorschemes.kanagawa.enable = true;
 
-        plugins = {
-            lsp = {
-                enable = true;
-                servers = {
-                    bashls.enable = true;
-                };
-	    telescope = {
-	    	enable = true;
-		keymaps = {
-		     "<leader>f" = "find_files";
+		plugins = {
+			lsp = {
+				enable = true;
+				servers = {
+					bashls.enable = true;
+				};
+				telescope = {
+					enable = true;
+					keymaps = {
+						"<leader>f" = "find_files";
+					};
+				};
+			};
 		};
-            };
-        };
-    };
+	};
 }
