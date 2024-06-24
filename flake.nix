a467299b5b1d74f1b5adc485d097f66659a403cd
Olisnot <oliverpm2007@gmail.com>
2024-06-24 07:14:25 +0100

    rebuild-24-06-2024 07:14:25

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
      };
 
-    inputs.nixvim = {
+    nixvim = {
         url = "github:nix-community/nixvim";
         inputs.nixpkgs.follows = "nixpkgs";
     };
