87779ccf106415c2ca391693fb8f87320fb73764
Olisnot <oliverpm2007@gmail.com>
2024-06-25 22:32:28 +0100

    rebuild-25-06-2024 22:32:28

--- modules/NixVim/Plugins/lsp.nix
+++ modules/NixVim/Plugins/lsp.nix
@@ -9,7 +9,10 @@
 					bashls.enable = true;
 					cmake.enable = true;
 					csharp-ls.enable = true;
-					pylsp.enable = true;
+					pylsp = {
+						enable = true;
+						autostart = true;
+					};
 					rust-analyzer = { 
 						enable = true;
 						installCargo = true;
