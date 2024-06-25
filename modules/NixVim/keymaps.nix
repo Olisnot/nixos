4bdebf4794a911f1342400bfcce831ae1603eb1e
Olisnot <oliverpm2007@gmail.com>
2024-06-25 20:21:37 +0100

    rebuild-25-06-2024 20:21:37

--- modules/NixVim/keymaps.nix
+++ modules/NixVim/keymaps.nix
@@ -8,12 +8,12 @@
 			action = "<cmd>Ex<CR>";
 			key = "<leader>e";
 			mode = [ "n" ];
-		}
+		};
 		{
 			action = "function() builtin.grep_string({ search = vim.fn.input(\"Grep > \") }";
 			key = "<leader>gr";
 			mode = [ "n" ];
-		}
+		};
 		];
 	};
 }
