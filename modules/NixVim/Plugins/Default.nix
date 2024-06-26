9b19253aac5c9597f0fbe3c69b6fe955ae7117d9
Olisnot <oliverpm2007@gmail.com>
2024-06-26 23:36:20 +0100

    rebuild-26-06-2024 23:36:20

--- modules/NixVim/Plugins/Default.nix
+++ modules/NixVim/Plugins/Default.nix
@@ -8,5 +8,6 @@
 		./treesitter.nix
 		./coq.nix
 		./cursorline.nix
+		./obsidian.nix
 	];
 }
---
+++ modules/NixVim/Plugins/obsidian.nix
@@ -0,0 +1,19 @@
+{ pkgs, inputs, ... }: 
+
+{
+	programs.nixvim = {
+		plugins = {
+			obsidian = {
+				enable = true;
+				settings = {
+					workspaces = [
+					{
+						name = "vault";
+						path = "/mnt/1tb/Obsidian";
+					}
+					];
+				};
+			};
+		};
+	};
+}
