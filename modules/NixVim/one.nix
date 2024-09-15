4824da86e4182b6ba8650145582e0ad649a6cec7
Olisnot <oliverpm2007@gmail.com>
2024-09-15 18:49:47 +0100

    rebuild-15-09-2024 18:49:47

--- modules/NixVim/one.nix
+++ modules/NixVim/one.nix
@@ -2,19 +2,11 @@
 
 {
 	programs.nixvim = {
-		colorschemes.ayu = {
+		colorschemes.modus = {
 			enable = true;
 
-                        overrides = {
-                          Normal.bg = "None";
-                          ColorColumn.bg = "None";
-                          SignColumn.bg = "None";
-                          Folded.bg = "None";
-                          FoldColumn.bg = "None";
-                          CursorLine.bg = "None";
-                          CursorColumn.bg = "None";
-                          WhichKeyFloat.bg = "None";
-                          VertSplit.bg = "None";
+                        settings = {
+                          transparent = true;
                         };
 		};
 	};
