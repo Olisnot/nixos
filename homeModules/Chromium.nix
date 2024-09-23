b9b179a37acb66b9518ef4fad70fa89cde084f87
Olisnot <oliverpm2007@gmail.com>
2024-09-23 18:53:03 +0100

    rebuild-23-09-2024 18:53:03

--- homeModules/Chromium.nix
+++ homeModules/Chromium.nix
@@ -6,8 +6,16 @@
     package = pkgs.ungoogled-chromium;
 
     extensions = [
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } #Ublock origin
-      { id = "nngceckbapebfimnlniiiahkandclblb"; } #Bitwarden
+      { 
+        id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+        updateUrl = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=126.0&x=id%3Dcjpalhdlnbpafiamejdnhcphjbkeiagm%26installsource%3Dondemand%26uc";
+      } #Ublock origin
+
+      { 
+        id = "nngceckbapebfimnlniiiahkandclblb";
+        updateUrl = "https://clients2.google.com/service/update2/crx?response=redirect&acceptformat=crx2,crx3&prodversion=126.0&x=id%3Dnngceckbapebfimnlniiiahkandclblb%26installsource%3Dondemand%26uc";
+      } #Bitwarden
+
     ];
   };
 }
