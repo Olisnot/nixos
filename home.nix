ff6949ccec8bd54a9e14c32f5104d7a84f60b7e4
Olisnot <oliverpm2007@gmail.com>
2024-09-26 17:07:38 +0100

    rebuild-26-09-2024 17:07:38

--- home.nix
+++ home.nix
@@ -40,6 +40,12 @@
 		  name = "Tela-circle-dark";
 	  };
 
+          cursorTheme = {
+                  package = pkgs.graphite-cursors;
+                  name = "Graphite-Dark";
+                  size = 16;
+          };
+
 	  gtk3.extraConfig = {
 		  Settings = ''
 			  gtk-application-prefer-dark-theme=1
