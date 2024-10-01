d8164f36dac3e8e2f7445e5220edaf1eb6cc7001
Olisnot <oliverpm2007@gmail.com>
2024-10-02 00:52:15 +0100

    rebuild-02-10-2024 00:52:15

--- packages.nix
+++ packages.nix
@@ -66,7 +66,7 @@
 			gcc
 			cmake
                         dotnet-sdk_8
-                        dotnetCorePackages.sdk_8_0_1xx
+                        dotnetCorePackages.sdk_8_0_2xx
 			python39
 			lua
 
