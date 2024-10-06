4a05b1d7e7332c2547085853573502c648ee664a
Olisnot <oliverpm2007@gmail.com>
2024-10-06 17:01:58 +0100

    rebuild-06-10-2024 17:01:58

--- homeModules/Hypr/hyprland.nix
+++ homeModules/Hypr/hyprland.nix
@@ -72,8 +72,10 @@ general = {
   border_size = 1;
 
     # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
-    col.active_border = "rgba(fffafaaa)";
-    col.inactive_border = "rgba(595959aa)";
+    col = {
+      active_border = "rgba(fffafaaa)";
+      inactive_border = "rgba(595959aa)";
+    };
 
     # Set to true enable resizing windows by clicking and dragging on borders and gaps
     resize_on_border = false;
@@ -178,7 +180,7 @@ device = {
 ####################
 
 # See https://wiki.hyprland.org/Configuring/Keywords/
-mainMod = "SUPER"; # Sets "Windows" key as main modifier
+"$mainMod" = "SUPER"; # Sets "Windows" key as main modifier
 
 # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
 bind = [
