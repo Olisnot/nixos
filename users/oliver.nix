081b6c7710519e211d5e55ce7a4e320c73efc2f5
Olisnot <oliverpm2007@gmail.com>
2024-10-12 22:41:52 +0100

    rebuild-12-10-2024 22:41:52

--- users/oliver.nix
+++ users/oliver.nix
@@ -9,7 +9,7 @@
 	};
 
 	home-manager = { 
-		backupFileExtension = "backup17";
+		backupFileExtension = "backup18";
 		extraSpecialArgs = {inherit inputs;};
 		users = {
 			"oliver" = import ../home.nix;
