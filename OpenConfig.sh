77cb93fdbc65a2b4a0d12cf1a4a7a1db6ed9a81e
Olisnot <oliverpm2007@gmail.com>
2024-10-12 17:25:14 +0100

    rebuild-12-10-2024 17:25:14

--- OpenConfig.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd /nixos || exit
-tmux new-session -d -s NixOSConfig -n Rebuild
-tmux new-window -t NixOSConfig:2 -n NeoVim
-tmux send-keys -t NixOSConfig:2 'sudovim'
-tmux select-window -t Rebuild
-tmux a -t NixOSConfig
