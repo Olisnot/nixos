417ac107970bf5d73b813982b32834dfde181969
Olisnot <oliverpm2007@gmail.com>
2024-10-12 19:55:26 +0100

    rebuild-12-10-2024 19:55:26

--- homeModules/Firefox/Personal.nix
+++ homeModules/Firefox/Personal.nix
@@ -9,37 +9,31 @@
       {
         name = "YouTube";
         keyword = "youtube";
-        toolbar = true;
         url = "https://www.youtube.com/";
       }
       {
         name = "Twitch";
         keyword = "twitch";
-        toolbar = true;
         url = "http://www.twitch.tv/";
       }
       {
         name = "AllAnime";
         keyword = "anime";
-        toolbar = true;
         url = "https://allmanga.to/anime?tr=sub&cty=ALL";
       }
       {
         name = "AniList";
         keyword = "anilist";
-        toolbar = true;
         url = "https://anilist.co/home";
       }
       {
         name = "Reddit";
         keyword = "reddit";
-        toolbar = true;
         url = "https://www.reddit.com/";
       }
       {
         name = "GitHub";
         keyword = "github";
-        toolbar = true;
         url = "https://github.com/";
       }
     ];
