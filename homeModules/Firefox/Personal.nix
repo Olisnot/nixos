fd3c575e865d32eadf9bd79d580acf38b94c64f9
Olisnot <oliverpm2007@gmail.com>
2024-10-12 20:00:45 +0100

    rebuild-12-10-2024 20:00:44

--- homeModules/Firefox/Personal.nix
+++ homeModules/Firefox/Personal.nix
@@ -7,36 +7,40 @@
     search.default = "DuckDuckGo";
     bookmarks = [
       {
-        name = "YouTube";
         toolbar = true;
-        keyword = "youtube";
-        url = "https://www.youtube.com/";
-      }
-      {
-        name = "Twitch";
-        keyword = "twitch";
-        url = "http://www.twitch.tv/";
-      }
-      {
-        name = "AllAnime";
-        keyword = "anime";
-        url = "https://allmanga.to/anime?tr=sub&cty=ALL";
-      }
-      {
-        name = "AniList";
-        keyword = "anilist";
-        url = "https://anilist.co/home";
-      }
-      {
-        name = "Reddit";
-        keyword = "reddit";
-        url = "https://www.reddit.com/";
-      }
-      {
-        name = "GitHub";
-        keyword = "github";
-        url = "https://github.com/";
-      }
+        bookmarks = [
+          {
+            name = "YouTube";
+            keyword = "youtube";
+            url = "https://www.youtube.com/";
+          }
+          {
+            name = "Twitch";
+            keyword = "twitch";
+            url = "http://www.twitch.tv/";
+          }
+          {
+            name = "AllAnime";
+            keyword = "anime";
+            url = "https://allmanga.to/anime?tr=sub&cty=ALL";
+          }
+          {
+            name = "AniList";
+            keyword = "anilist";
+            url = "https://anilist.co/home";
+          }
+          {
+            name = "Reddit";
+            keyword = "reddit";
+            url = "https://www.reddit.com/";
+          }
+          {
+            name = "GitHub";
+            keyword = "github";
+            url = "https://github.com/";
+          }
+        ];
+    }
     ];
 
     extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
