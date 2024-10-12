a1182f330cf3a02dcf4ac58e1c29cb0cf9f95840
Olisnot <oliverpm2007@gmail.com>
2024-10-12 17:29:06 +0100

    rebuild-12-10-2024 17:29:05

--- modules/aliases.nix
+++ modules/aliases.nix
@@ -6,7 +6,6 @@
         sudovim = "sudo -E nvim .";
         vim = "nvim .";
         tb = "cd /mnt/1tb";
-        config = "/nixos/OpenConfig.sh";
         cleanGenerations = "/nixos/trim-generations.sh";
         Obsidian = "cd /home/oliver/Obsidian && tmux new-session -A -s Obsidian";
     };
