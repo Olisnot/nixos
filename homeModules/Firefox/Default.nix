04f620b7fad567c335a33107707c3ae829c604d0
Olisnot <oliverpm2007@gmail.com>
2024-10-12 22:44:42 +0100

    rebuild-12-10-2024 22:44:42

--- homeModules/Firefox/Default.nix
+++ homeModules/Firefox/Default.nix
@@ -4,6 +4,7 @@
   
   imports = [ 
     ./Personal.nix
+    ./Work.nix
   ];
   programs.firefox = { 
     enable = true;
--- homeModules/Firefox/Work.nix
+++ homeModules/Firefox/Work.nix
@@ -1,4 +1,27 @@
-{ ... }:
+{ inputs, ... }:
 
 {
+  programs.firefox.profiles.Work = {
+    name = "Work";
+    isDefault = true;
+    search.default = "DuckDuckGo";
+    bookmarks = [
+      {
+        toolbar = true;
+        bookmarks = [
+        ];
+    }
+    ];
+
+    extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+        bitwarden
+        ublock-origin
+        youtube-shorts-block
+      ];
+
+    settings = {
+      "browser.toolbars.bookmarks.visibility" = "always";
+      "browser.startup.page" = 3;
+    };
+  };
 }
