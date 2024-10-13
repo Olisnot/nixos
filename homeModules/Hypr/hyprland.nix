9682163b172457e3f67ad6c4cc7406024eda14a2
Olisnot <oliverpm2007@gmail.com>
2024-10-13 19:03:40 +0100

    rebuild-13-10-2024 19:03:40

--- homeModules/Hypr/hyprland.nix
+++ homeModules/Hypr/hyprland.nix
@@ -150,7 +150,7 @@ misc = {
 # https://wiki.hyprland.org/Configuring/Variables/#input
 input = {
   kb_layout = "us, us";
-  kb_variant = ", dvorak";
+  kb_variant = "qwerty, dvorak";
 
   follow_mouse = 0;
 
