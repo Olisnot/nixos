fa246d57b9ea964cdf654c1952e32094ef9e547b
Olisnot <oliverpm2007@gmail.com>
2024-10-16 18:00:06 +0100

    rebuild-16-10-2024 18:00:06

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,8 @@
 
 	nix.settings.experimental-features = ["nix-command" "flakes"];
 
+        nix.settings.auto-optimise-store = true;
+
 # Bootloader.
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
