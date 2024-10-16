70c72d90007ad544f4457b187a114f48f7795efd
Olisnot <oliverpm2007@gmail.com>
2024-10-16 21:11:01 +0100

    rebuild-16-10-2024 21:11:01

--- modules/Gaming/aagl.nix
+++ modules/Gaming/aagl.nix
@@ -6,7 +6,7 @@ in
     aagl-gtk-on-nix.module
   ];
 
-  programs.anime-game-launcher.enable = true;
+  #programs.anime-game-launcher.enable = true;
   programs.honkers-railway-launcher.enable = true;
   #programs.wavey-launcher.enable = true;
   programs.sleepy-launcher.enable = true;
