2d0fe2c0a51133cfcf0d07eb82a041a8c16c69ca
Olisnot <oliverpm2007@gmail.com>
2024-10-16 12:57:19 +0100

    rebuild-16-10-2024 12:57:19

--- scripts/default.nix
+++ scripts/default.nix
@@ -1,8 +1,8 @@
 { pkgs, inputs, ... }:
 
 let
-  rebuild = import ./scripts/Rebuild.nix { inherit pkgs; };
-  config = import ./scripts/Config.nix { inherit pkgs; };
+  rebuild = import ./Rebuild.nix { inherit pkgs; };
+  config = import ./Config.nix { inherit pkgs; };
   nvim = inputs.nixvim.packages."x86_64-linux".default;
 in
 {
