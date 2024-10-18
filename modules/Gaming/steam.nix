1191e9b09fdeaf521265504b4731958ebccf3793
Olisnot <oliverpm2007@gmail.com>
2024-10-19 00:53:07 +0100

    rebuild-19-10-2024 00:53:07

--- modules/Gaming/steam.nix
+++ modules/Gaming/steam.nix
@@ -1,11 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
 	programs.steam = {
 		enable = true;
 		remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-			dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-			gamescopeSession.enable = true;
+                dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+                gamescopeSession.enable = true;
+
+                extraCompatPackages = [
+                  pkgs.proton-ge-bin
+                ];
 	};
+
+        gamescope = {
+          enable = true;
+          capSysNice = true;
+          args = [
+            "--rt"
+            "--expose-wayland"
+          ];
+        };
 
 }
