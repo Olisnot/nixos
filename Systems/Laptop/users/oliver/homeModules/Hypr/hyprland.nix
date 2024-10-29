bd16a418d7c863ae67c9eec557fe8d586695a0f6
Olisnot <oliverpm2007@gmail.com>
2024-10-29 15:26:48 +0000

    rebuild-29-10-2024 15:26:48

--- Systems/Laptop/users/oliver/homeModules/Hypr/hyprland.nix
+++ Systems/Laptop/users/oliver/homeModules/Hypr/hyprland.nix
@@ -10,8 +10,8 @@
 
 # See https://wiki.hyprland.org/Configuring/Monitors/
 monitor= [ 
-  "DP-3,1920x1080,0x1080,1"
-  "DP-4,1920x1080,19200x1080,1"
+  "DP-1,1920x1080,0x1080,1"
+  "HDMI-A-1,1920x1080,19200x1080,1"
   "eDP-1,1920x1080,192000x0,1"
   "Unknown-1, disable"
 ];
@@ -325,15 +325,15 @@ windowrulev2 = [
 ];
 
 workspace= [ 
-  "1,monitor:DP-1"
-  "2,monitor:DP-1"
-  "3,monitor:DP-1"
-  "4,monitor:DP-1"
-  "5,monitor:DP-1"
-  "6,monitor:HDMI-A-1"
-  "7,monitor:HDMI-A-1"
-  "8,monitor:HDMI-A-1"
-  "9,monitor:HDMI-A-1"
+  "1,monitor:DP-3"
+  "2,monitor:DP-3"
+  "3,monitor:DP-3"
+  "4,monitor:DP-3"
+  "5,monitor:DP-3"
+  "6,monitor:DP-4"
+  "7,monitor:DP-4"
+  "8,monitor:DP-4"
+  "9,monitor:DP-4"
   "10,monitor:eDP-1"
 ];
   };
