d25527cc3672e447af108fe9a69530d40e88b776
Olisnot <oliverpm2007@gmail.com>
2024-10-31 19:12:17 +0000

    rebuild-31-10-2024 19:12:17

--- Systems/Laptop/users/oliver/homeModules/Floorp/personal.nix
+++ Systems/Laptop/users/oliver/homeModules/Floorp/personal.nix
@@ -59,6 +59,10 @@
       "floorp.browser.sidebar2.data" = ''
       {"data":{"floorp__history":{"url":"floorp//history","width":415},"floorp__downloads":{"url":"floorp//downloads","width":415},"floorp__profiles":{"url":"about:profiles","width":400}},"index":["floorp__profiles", "floorp__history","floorp__downloads"]}
       '';
+
+      "browser.uiCustomization.state" = ''
+      {"placements":{"widget-overflow-fixed-list":[],"unified-extensions-area":[],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring1","urlbar-container","customizableui-special-spring2","save-to-pocket-button","downloads-button","unified-extensions-button","profile-manager","ublock0_raymondhill_net-browser-action","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["personal-bookmarks"],"statusBar":["screenshot-button","fullscreen-button","status-text"]},"seen":["developer-button","sidebar-reverse-position-toolbar","undo-closed-tab","profile-manager","workspaces-toolbar-button","_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action","ublock0_raymondhill_net-browser-action"],"dirtyAreaCache":["nav-bar","statusBar","TabsToolbar","unified-extensions-area","toolbar-menubar","PersonalToolbar"],"currentVersion":20,"newElementCount":2}
+      '';
     };
   };
 }
