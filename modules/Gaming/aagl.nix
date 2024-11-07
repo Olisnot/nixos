6833f7568bd9f7bb7977bb4ea65cbf3c0c69cdef
Olisnot <oliverpm2007@gmail.com>
2024-11-07 16:26:53 +0000

    rebuild-07-11-2024 16:26:53

--- modules/Gaming/aagl.nix
+++ modules/Gaming/aagl.nix
@@ -1,7 +1,8 @@
+{ lib, config, ... }:
+
 let
   aagl-gtk-on-nix = import (builtins.fetchTarball "https://github.com/ezKEa/aagl-gtk-on-nix/archive/main.tar.gz");
 in
-  { lib, config }:
   {
     imports = [
       aagl-gtk-on-nix.module
