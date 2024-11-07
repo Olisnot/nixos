6760ba763996dbf44d462dc5cda8357182a034fa
System administrator <root@nixos.>
2024-11-07 19:55:44 +0000

    rebuild-07-11-2024 19:55:44

--- Systems/Laptop/aliases.nix
+++ Systems/Laptop/aliases.nix
@@ -1,17 +1,12 @@
-{ config, lib, ... }:
+{ ... }:
 
 {
-  options = {
-    aliases.enable = lib.mkEnableOption "Enables Bash aliases";
-  };
 
-  config = lib.mkIf config.aliases.enable {
-    programs.bash.shellAliases = {
-        c = "clear";
-        sudovim = "sudo -E nvim .";
-        vim = "nvim .";
-        tb = "cd /mnt/1tb";
-        Obsidian = "cd /home/oliver/Obsidian && tmux new-session -A -s Obsidian";
-    };
+  programs.bash.shellAliases = {
+    c = "clear";
+    sudovim = "sudo -E nvim .";
+    vim = "nvim .";
+    tb = "cd /mnt/1tb";
+    Obsidian = "cd /home/oliver/Obsidian && tmux new-session -A -s Obsidian";
   };
 }
--- Systems/WSL/aliases.nix
+++ Systems/WSL/aliases.nix
@@ -1,15 +1,10 @@
-{ config, lib, ... }:
+{ ... }:
 
 {
-  options = {
-    aliases.enable = lib.mkEnableOption "Enables Bash aliases";
-  };
 
-  config = lib.mkIf config.aliases.enable {
-    programs.bash.shellAliases = {
-        c = "clear";
-        sudovim = "sudo -E nvim .";
-        vim = "nvim .";
-    };
+  programs.bash.shellAliases = {
+    c = "clear";
+    sudovim = "sudo -E nvim .";
+    vim = "nvim .";
   };
 }
