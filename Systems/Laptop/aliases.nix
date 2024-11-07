2862aa9f6bf04f6e82cc379c68628e37d596f0b1
Olisnot <oliverpm2007@gmail.com>
2024-11-07 20:38:44 +0000

    rebuild-07-11-2024 20:38:44

--- Systems/Laptop/aliases.nix
+++ Systems/Laptop/aliases.nix
@@ -1,11 +1,6 @@
-{ config, lib, ... }:
+{ ... }:
 
 {
-  options = {
-    aliases.enable = lib.mkEnableOption "Enables Bash aliases";
-  };
-
-  config = lib.mkIf config.aliases.enable {
     programs.bash.shellAliases = {
         c = "clear";
         sudovim = "sudo -E nvim .";
@@ -13,5 +8,4 @@
         tb = "cd /mnt/1tb";
         Obsidian = "cd /home/oliver/Obsidian && tmux new-session -A -s Obsidian";
     };
-  };
 }
