ff911e5a2c7d1b2fe24ccd391f20b735c8439d9a
Olisnot <oliverpm2007@gmail.com>
2024-11-07 15:52:06 +0000

    rebuild-07-11-2024 15:52:06

--- Systems/Laptop/users/oliver/user.nix
+++ Systems/Laptop/users/oliver/user.nix
@@ -9,7 +9,7 @@
 	};
 
 	home-manager = { 
-                backupFileExtension = "backup68";
+                backupFileExtension = "backup69";
 		extraSpecialArgs = {inherit inputs;};
                 useGlobalPkgs = true;
 		users = {
