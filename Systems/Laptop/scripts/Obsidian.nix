078ee60fd75cb3f3aadbb44fff9be006b42cd668
Olisnot <oliverpm2007@gmail.com>
2024-11-10 01:02:43 +0000

    rebuild-10-11-2024 01:02:43

--- Systems/Laptop/scripts/Obsidian.nix
+++ Systems/Laptop/scripts/Obsidian.nix
@@ -4,9 +4,11 @@ pkgs.writeShellApplication {
   name = "Obsidian";
   text = /* bash */ ''
   cd /home/oliver/Obsidian
-  tmux new-session -A -s Obsidian -n Editing
+  tmux new-session -d -s Obsidian -n Editing
+  tmux new-window -t Obsidian:2 -n Git
   tmux new-window -t Obsidian:2 -n Git
   tmux send -t Obsidian:1 'nix develop' Enter
+  tmux select-window -t Editing
   tmux a -t Obsidian
   '';
 }
