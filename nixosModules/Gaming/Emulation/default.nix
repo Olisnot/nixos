c2383d1a2ca63ae5169fd6431a433bb33c7b6d2c
Olisnot <oliverpm2007@gmail.com>
2024-11-11 16:20:47 +0000

    rebuild-11-11-2024 16:20:46

--- nixosModules/Gaming/Emulation/default.nix
+++ nixosModules/Gaming/Emulation/default.nix
@@ -3,10 +3,10 @@
 {
   imports = [
     ./emulation.nix
-    ./3ds.nix
-    ./playstation.nix
+    #./3ds.nix
+    #./playstation.nix
     ./playstation2.nix
-    ./wii-gamecube.nix
-    ./wiiu.nix
+    #./wii-gamecube.nix
+    #./wiiu.nix
   ];
 }
