c2674b5ffb4165b47e1f346ddd9f30be0814a917
Olisnot <oliverpm2007@gmail.com>
2024-11-27 22:04:12 +0000

    rebuild-27-11-2024 22:04:12

--- nixosModules/Gaming/steam.nix
+++ nixosModules/Gaming/steam.nix
@@ -11,7 +11,7 @@
         enable = true;
         remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        gamescopeSession.enable = true;
+        #gamescopeSession.enable = true;
 
         extraCompatPackages = [
           pkgs.proton-ge-bin
