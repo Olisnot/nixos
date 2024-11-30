de44eb3f3fbad6bef413da82fecf3658e405b9dc
Olisnot <oliverpm2007@gmail.com>
2024-11-30 13:49:26 +0000

    rebuild-30-11-2024 13:49:26

--- homeModules/alacritty.nix
+++ homeModules/alacritty.nix
@@ -9,36 +9,44 @@
     programs.alacritty = {
       enable = true;
       settings = {
-        colors.primary = {
-          background = "#1a1b26";
-          foreground = "#c0caf5";
-        };
+        # Kanagawa Alacritty Colors
+        colors = {
+          primary = {
+            background = "0x1f1f28";
+            foreground = "0xdcd7ba";
+          };
 
-        colors.cursor = {
-          cursor = "#c0caf5";
-          text = "#1a1b26";
-        };
+          normal = {
+            black = "0x090618";
+            red = "0xc34043";
+            green = "0x76946a";
+            yellow = "0xc0a36e";
+            blue = "0x7e9cd8";
+            magenta = "0x957fb8";
+            cyan = "0x6a9589";
+            white = "0xc8c093";
+          };
 
-        colors.normal = {
-          black = "#15161e";
-          red = "#f7768e";
-          green = "#9ece6a";
-          yellow = "#e0af68";
-          blue = "#7aa2f7";
-          magenta = "#bb9af7";
-          cyan = "#7dcfff";
-          white = "#a9b1d6";
-        };
+          bright = {
+            black = "0x727169";
+            red = "0xe82424";
+            green = "0x98bb6c";
+            yellow = "0xe6c384";
+            blue = "0x7fb4ca";
+            magenta = "0x938aa9";
+            cyan = "0x7aa89f";
+            white = "0xdcd7ba";
+          };
 
-        colors.bright = {
-          black = "#414868";
-          red = "#f7768e";
-          green = "#9ece6a";
-          yellow = "#e0af68";
-          blue = "#7aa2f7";
-          magenta = "#bb9af7";
-          cyan = "#7dcfff";
-          white = "#c0caf5";
+          selection = {
+            background = "0x2d4f67";
+            foreground = "0xc8c093";
+          };
+
+          indexed_colors = [
+            { index = 16; color = "0xffa066"; }
+            { index = 17; color = "0xff5d62"; }
+          ];
         };
       };
     };
