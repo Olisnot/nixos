240c60bfe6856142fed6cafd3b0e72b1be8a3fcc
Olisnot <oliverpm2007@gmail.com>
2025-02-28 23:54:06 +0000

    catpuccin mocha

--- nixosModules/stylix.nix
+++ nixosModules/stylix.nix
@@ -6,22 +6,22 @@
     image = "${inputs.wallpapers}/nord/ign_legendary.png";
     polarity = "dark";
     base16Scheme = {
-      base00 = "#2E3440";
-      base01 = "#3B4252";
-      base02 = "#434C5E";
-      base03 = "#4C566A";
-      base04 = "#D8DEE9";
-      base05 = "#E5E9F0";
-      base06 = "#ECEFF4";
-      base07 = "#8FBCBB";
-      base08 = "#BF616A";
-      base09 = "#D08770";
-      base0A = "#EBCB8B";
-      base0B = "#A3BE8C";
-      base0C = "#88C0D0";
-      base0D = "#81A1C1";
-      base0E = "#B48EAD";
-      base0F = "#5E81AC";
+      base00 = "#1e1e2e"; # base
+      base01 = "#181825"; # mantle
+      base02 = "#313244"; # surface0
+      base03 = "#45475a"; # surface1
+      base04 = "#585b70"; # surface2
+      base05 = "#cdd6f4"; # text
+      base06 = "#f5e0dc"; # rosewater
+      base07 = "#b4befe"; # lavender
+      base08 = "#f38ba8"; # red
+      base09 = "#fab387"; # peach
+      base0A = "#f9e2af"; # yellow
+      base0B = "#a6e3a1"; # green
+      base0C = "#94e2d5"; # teal
+      base0D = "#89b4fa"; # blue
+      base0E = "#cba6f7"; # mauve
+      base0F = "#f2cdcd"; # flamingo
     };
 
     fonts = {
@@ -50,7 +50,7 @@
 
     cursor = {
       package = pkgs.capitaine-cursors-themed;
-      name = "Capitaine Cursors (Nord)";
+      name = "Capitaine Cursors";
       size = 25;
     };
   };
